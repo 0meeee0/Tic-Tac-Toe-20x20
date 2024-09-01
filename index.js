b375aa368837ea0bbc962d391b9ba6116ed00172
0meeee0 <mehdi74.id@gmail.com>
2024-09-01 05:03:20 +0100

    Feat: Implement X and O functionality with sound effects, no checks for wins yet

--- index.js
+++ index.js
@@ -1,11 +1,24 @@
 document.body.onload = cv
 let oui = document.getElementById("board")
-function addTest() {
-    let newDiv = document.createElement('div')
-    newDiv.textContent = 'This is a test div'
-    newDiv.style.backgroundColor = 'yellow'
-    newDiv.style.padding = '100px'
-    oui.appendChild(newDiv)
+let p1Name = document.getElementById("p1Name").textContent = localStorage.getItem("P1")
+let p2Name = document.getElementById("p2Name").textContent = localStorage.getItem("P2")
+console.log(p1Name)
+
+let xAudio = new Audio("../src/x.mp3")
+let oAudio = new Audio("../src/o.mp3")
+function playMusic() {
+    let audio = new Audio("../src/urf.mp3");
+    audio.volume = 0.2;
+    audio.play();
+}
+function cv() {
+    for (let i = 0; i < 400; i++) {
+        let morba3 = document.createElement("div");
+        morba3.classList.add("morba3");
+        oui.appendChild(morba3);
+    }
+    playMusic()
+    Xmark();
 }
 
 function checkPlayers(){
@@ -22,11 +35,26 @@ function checkPlayers(){
     return true
 }
 
-
-function cv() {
-    for(let i=0; i<400; i++) {
-        let morba3 = document.createElement("div")
-        morba3.classList.add("morba3")
-        oui.appendChild(morba3)
+let cxo = "X";
+function Xmark(){
+    let buttons = document.querySelectorAll(".morba3");
+    // console.log(buttons)
+    buttons.forEach((btn) => {
+        btn.addEventListener("click", function () {
+            if (btn.textContent == "") {
+                console.log(cxo)
+                btn.textContent = cxo;
+                cxo = (cxo == "X") ? "O" : "X";
+                xoAudio()
+                console.log(cxo)
+            }
+        });
+    })
+}
+function xoAudio(){
+    if(cxo == "X"){
+        xAudio.play()
+    }else{
+        oAudio.play()
     }
 }
